aba8722c293918ae3921dda8ca3a0a973cc9adbc
Sergio Serrato <serrato@cal.berkeley.edu>
2015-06-29 12:47:37 -0700

    tic tac toe attempt

---
+++ app.js
@@ -0,0 +1,36 @@
+var boxContent = document.querySelectorAll(".box");
+var div_content = document.querySelector('.box');
+var oX = document.getElementsByClassName(".box");
+var tryAgain = document.querySelectorAll(".box");
+var boxGet = document.querySelectorAll(".box");
+var singleQuote = document.querySelectorAll('.box')
+    var oneBox = document.getElementById('#one');
+var twoBox
+var threeBox
+alert ("A Long Preparation. A Disappointing Failure. A Painful Lesson Learned ");
+console.log(tryAgain);
+console.log(oneBox);
+console.log(oX);
+// function youClick(){
+//   console.log(5+6);
+// }
+// for (var i = 0; i < array.length; i++) {
+//   array[i]
+// }
+//
+// this.innerhtml
+
+// counter[].addEventListener("click", youClick);
+
+// boxVar.addEventListener("mouseenter", tripleIt(3));
+div_content.addEventListener('mouseenter', setList(2));
+    function thisInside() {
+    console.log("thisInside");
+}
+
+function setList(x) {
+  return 5 + x;
+}
+console.log(setList);
+
+// need to understand inner html
